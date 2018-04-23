89018c683f45de53e675b46778d4f146b69a8608
Almaz <hamedzhanovalmaz@gmail.com>
2018-04-23 21:07:53 +0300

    Add SumThread
    Only sum with "for"

--- SumThread/src/ru/itis/Main.java
+++ SumThread/src/ru/itis/Main.java
@@ -1,8 +1,25 @@
 package ru.itis;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+
+        int countOfNumbers = scanner.nextInt();
+        int countOfThreads = scanner.nextInt();
+
+	    int arr[] = new int[countOfNumbers];
+	    int sumFor = 0;
+	    int sumThreads = 0;
+
+	    for(int i = 0; i < countOfNumbers; i++) {
+	        arr[i] = (int)(Math.random() * 1000);
+            System.out.println(arr[i]);
+            sumFor += arr[i];
+        }
+        System.out.println(sumFor);
+
     }
 }
