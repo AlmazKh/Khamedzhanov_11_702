de9f1abe5ee3bdb48b3f745d99f95c3258cad88f
Almaz <hamedzhanovalmaz@gmail.com>
2018-04-24 21:54:30 +0300

    Add SumThread

--- SumThread/src/ru/itis/Main.java
+++ SumThread/src/ru/itis/Main.java
@@ -4,13 +4,14 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Scanner scanner = new Scanner(System.in);
 
         int countOfNumbers = scanner.nextInt();
         int countOfThreads = scanner.nextInt();
 
 	    int arr[] = new int[countOfNumbers];
+
 	    int sumFor = 0;
 	    int sumThreads = 0;
 
@@ -20,6 +21,18 @@ public class Main {
             sumFor += arr[i];
         }
         System.out.println(sumFor);
+
+        Threads[] threads = new Threads[countOfThreads];
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Threads((countOfNumbers / countOfThreads) * i, (countOfNumbers / countOfThreads) * (i + 1), arr);
+            threads[i].start();
+        }
+
+        for (int i = 0; i < threads.length; i++) {
+            threads[i].join();
+            sumThreads += threads[i].getSum();
+        }
+        System.out.println(sumThreads);
 
     }
 }
--- SumThread/src/ru/itis/Threads.java
+++ SumThread/src/ru/itis/Threads.java
@@ -1,5 +1,27 @@
 package ru.itis;
 
 public class Threads extends Thread{
+    private int start;
+    private int end;
+    private int[] array;
+    private int sum = 0;
 
+    public Threads(int start, int end, int[] arr) {
+        super();
+        this.start = start;
+        this.end = end;
+        this.array = arr;
+    }
+
+    @Override
+    public void run() {
+        for (int i = start; i < end; i++) {
+            System.out.println(Thread.currentThread().getName());
+            sum += array[i];
+        }
+    }
+
+    public int getSum() {
+        return sum;
+    }
 }
