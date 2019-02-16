bc647fad318a33205ac9d303dc4b8c484225055e
AlmazKh <32297268+AlmazKh@users.noreply.github.com>
2019-02-16 22:35:11 +0300

    Add WebListener which give a new bug
    Error during artifact deployment

--- Hospitals/src/main/java/ru/itis/listeners/ContextListener.java
+++ Hospitals/src/main/java/ru/itis/listeners/ContextListener.java
@@ -6,8 +6,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 import javax.sql.DataSource;
 
+@WebListener
 public class ContextListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
